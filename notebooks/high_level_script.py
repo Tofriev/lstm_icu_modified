48acd11159068e3d72e9dcc7924ad3b16fd723ea
Tofriev <t.voelkel@posteo.de>
2025-02-25 16:34:40 +0100

    new env and some cleaning

--- notebooks/high_level_script.py
+++ notebooks/high_level_script.py
@@ -6,54 +6,9 @@ from utils import set_seed
 import json
 import os
 from classes.pipeline import MultiDatasetPipeline
+from start_utils import variables, count_features
 
 set_seed(42)
-
-# high level functionalities work maily for mimic (yet)
-
-
-variables = {
-    "mbp": {"type": "numerical", "training": True},  # Mean Blood Pressure
-    "gcs_total": {"type": "numerical", "training": True},  # Glasgow Coma Scale Total
-    "glc": {"type": "numerical", "training": True},  # Glucose levels
-    # "resprate_mortality": {"type": "numerical", "training": True},  # Respiratory Rate TODO: extract resprate. we dont need mortality here anymore
-    "creatinine": {"type": "numerical", "training": True},  # Creatinine levels
-    "potassium": {"type": "numerical", "training": True},  # Potassium levels
-    "hr": {"type": "numerical", "training": True},  # Heart Rate
-    # "sodium": {"type": "numerical", "training": True},  # Sodium levels
-    "wbc": {"type": "numerical", "training": True},  # White Blood Cells (leukocytes)
-    "platelets": {"type": "numerical", "training": True},  # Platelets (thrombocytes)
-    "inr": {
-        "type": "numerical",
-        "training": True,
-    },  # International Normalized Ratio (Prothrombin Time)
-    "anion_gap": {"type": "numerical", "training": True},  # Anion Gap
-    "lactate": {"type": "numerical", "training": True},  # Lactate levels
-    # "urea": {"type": "numerical", "training": True},  # Urea levels
-    "temperature": {"type": "numerical", "training": True},  # Body Temperature
-    "weight": {"type": "numerical", "training": True},  # Weight over time (time series)
-    "static_data": {
-        "mortality": {"type": "target", "training": False},  # Mortality outcome
-        "age": {"type": "numerical", "training": True},  # Age
-        "gender": {"type": "categorical", "training": True},  # Gender
-        #'height': {'type': 'numerical', 'training': True},     # Height
-        "intime": {"type": "datetime", "training": False},
-        "first_day_end": {"type": "datetime", "training": False},
-        "stay_id": {"type": "id", "training": False},
-    },
-}
-
-
-def count_features(variables):
-    exclude_keys = {"mortality", "intime", "first_day_end", "stay_id", "static_data"}
-
-    top_level_keys = [key for key in variables if key not in exclude_keys]
-    count = len(top_level_keys)
-    count += len([key for key in variables["static_data"] if key not in exclude_keys])
-
-    return count
-
-
 n_features = count_features(variables)
 
 
